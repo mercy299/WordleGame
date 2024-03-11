a420119e2f4e3a540ff121a2a29ede765b5f055c
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2024-03-11 09:51:24 +0100

    add closing fi and gitignore file

--- program/execute.sh
+++ program/execute.sh
@@ -6,7 +6,8 @@ output=$(python read.py)
 # Print the captured output
 echo "$output"
 
-if [[ $output == *"$phrase_to_search"* ]]; then
-    echo "The phrase '$phrase_to_search' was found in the output."
-else
-    echo "The phrase '$phrase_to_search' was not found in the output."
+# if [[ $output == *"$phrase_to_search"* ]]; then
+#     echo "The phrase '$phrase_to_search' was found in the output."
+# else
+#     echo "The phrase '$phrase_to_search' was not found in the output."
+# fi
